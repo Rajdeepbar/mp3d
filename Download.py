5ce31ab4b209eb1721047ab825d66f89cae9b45f
siddhartha.346 <siddhartha.346@outlook.com>
2020-06-19 23:04:38 +0530

    1. Added resources folder 2. The mp3 files will now get stored in Downloads folder.

--- Download.py
+++ Download.py
@@ -1,4 +1,5 @@
 import youtube_dl 
+import os
 
 flag = 0
 
@@ -26,6 +27,14 @@ class download():
 
     def start(self,name):
         global flag
+        dflag = 0
+        dir_path = os.path.dirname(os.path.realpath(__file__)) 
+        for dirs in os.listdir(dir_path): 
+            if dirs == 'Downloads':
+                dflag = 1
+                break
+        if dflag != 1:
+            os.mkdir(os.getcwd()+'\Downloads')
         option = int(name)
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([self.url[option-1]])
